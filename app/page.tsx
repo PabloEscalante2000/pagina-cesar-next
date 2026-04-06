3dc72c0720c472e97cb788f426c307b0fad71ca7
Pablo Escalante <pabloescalante200027@gmail.com>
2026-04-06 17:29:01 -0500

    cambio final por hoy

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 export const dynamic = "force-static";
 
-import Footer from "@/components/Footer";
 import Landing from "@/components/Landing";
 
 export default function Home() {
-  return (
-    <>
-      <Landing />
-      <Footer />
-    </>
-  );
+  return <Landing />;
 }
